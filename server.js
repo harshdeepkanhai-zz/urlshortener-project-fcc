bcc4e9f80994cb0a45fe010ff9c915c0612309a5
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-06-18 09:35:37 +0530

    Add urlEntries model

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require("express");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
 
-var cors = require('cors');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+// Basic Configuration
+const mongoURL = process.env.MONGOLAB_URI;
+const port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+/** this project needs a db !! **/
+
+mongoose.connect(mongoURL);
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => res.sendFile(process.cwd() + "/views/index.html"));
+
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// Answer not found to all the wrong routes
+app.use((req, res, next) => {
+  res.status(404);
+  res.type("txt").send("Not found");
 });
 
-
-app.listen(port, function () {
-  console.log('Node.js listening ...');
-});
+app.listen(port, () => console.log(`Node.js listening ... on port ${port}`));
